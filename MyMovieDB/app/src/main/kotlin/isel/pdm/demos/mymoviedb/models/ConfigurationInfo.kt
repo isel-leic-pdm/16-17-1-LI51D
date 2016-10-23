4fce19b31132ef236ff315cf631cc14c57e4a1c6
Paulo Pereira <palbp.giga@gmail.com>
2016-10-24 00:52:16 +0100

    Added Parcelable implementation to existing DTOs

--- MyMovieDB/app/src/main/kotlin/isel/pdm/demos/mymoviedb/models/ConfigurationInfo.kt
+++ MyMovieDB/app/src/main/kotlin/isel/pdm/demos/mymoviedb/models/ConfigurationInfo.kt
@@ -1,22 +1,116 @@
 package isel.pdm.demos.mymoviedb.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * Class whose instances represent configuration information obtained from the remote API.
+ *
+ * <p>As all other DTOs used in the application, the class implements the Parcelable contract
+ * thereby allowing them to be passed between Android components and, eventually, across process
+ * boundaries.</p>
+ *
+ * @property images The configuration info required for obtaining images
+ * @property changeKeys ...
  */
 data class ConfigurationInfo(
         val images: Images,
-        @JsonProperty("change_keys") val changeKeys: Collection<String>
-    ) {
+        @JsonProperty("change_keys") val changeKeys: List<String>) : Parcelable {
 
+    /**
+     * Class whose instances represent movie collections in the enclosing movie detail instance.
+     *
+     * @property baseUrl The base URL (HTTP) used to build images URLs
+     * @property secureBase The base URL (HTTPS) used to build images URLs
+     * @property backdropSizes The supported sizes of backdrop images
+     * @property logoSizes The supported logo sizes
+     * @property posterSizes The supported sizes of poster images
+     * @property profileSizes The supported sizes of profile images
+     * @property stillSizes The supported sizes of still images
+     */
     data class Images(
             @JsonProperty("base_url") val baseUrl: String?,
             @JsonProperty("secure_base") val secureBase: String?,
-            @JsonProperty("backdrop_sizes") val backdropSizes: Collection<String>,
-            @JsonProperty("logo_sizes") val logoSizes: Collection<String>,
-            @JsonProperty("poster_sizes") val posterSizes: Collection<String>,
-            @JsonProperty("profile_sizes") val profileSizes: Collection<String>,
-            @JsonProperty("still_sizes") val stillSizes: Collection<String>
+            @JsonProperty("backdrop_sizes") val backdropSizes: List<String>,
+            @JsonProperty("logo_sizes") val logoSizes: List<String>,
+            @JsonProperty("poster_sizes") val posterSizes: List<String>,
+            @JsonProperty("profile_sizes") val profileSizes: List<String>,
+            @JsonProperty("still_sizes") val stillSizes: List<String>) : Parcelable {
+
+        companion object {
+            /** Factory of Images instances */
+            @JvmField @Suppress("unused")
+            val CREATOR = object : Parcelable.Creator<Images> {
+                override fun createFromParcel(source: Parcel) = Images(source)
+                override fun newArray(size: Int): Array<Images?> = arrayOfNulls(size)
+            }
+        }
+
+        /**
+         * Initiates an instance from the given parcel.
+         * @param source The parcel from where the data is to be loaded from
+         */
+        constructor(source: Parcel) : this(
+            baseUrl = source.readString(),
+            secureBase = source.readString(),
+            backdropSizes = source.createStringArrayList(),
+            logoSizes = source.createStringArrayList(),
+            posterSizes = source.createStringArrayList(),
+            profileSizes = source.createStringArrayList(),
+            stillSizes = source.createStringArrayList()
+        )
+
+        /** Not used (see android documentation for further details) */
+        override fun describeContents() = 0
+
+        /**
+         * Saves the instance data to the given parcel.
+         * @param dest The parcel to where the data is to be saved to
+         * @param flags Not used (see android documentation for further details)
+         */
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+
+            with(dest) {
+                writeString(baseUrl)
+                writeString(secureBase)
+                writeStringList(backdropSizes)
+                writeStringList(logoSizes)
+                writeStringList(posterSizes)
+                writeStringList(profileSizes)
+                writeStringList(stillSizes)
+            }
+        }
+    }
+
+    companion object {
+        /** Factory of Images instances */
+        @JvmField @Suppress("unused")
+        val CREATOR = object : Parcelable.Creator<ConfigurationInfo> {
+            override fun createFromParcel(source: Parcel) = ConfigurationInfo(source)
+            override fun newArray(size: Int): Array<ConfigurationInfo?> = arrayOfNulls(size)
+        }
+    }
+
+    /**
+     * Initiates an instance from the given parcel.
+     * @param source The parcel from where the data is to be loaded from
+     */
+    constructor(source: Parcel) : this(
+        source.readTypedObject(Images.CREATOR),
+        source.createStringArrayList()
     )
+
+    /** Not used (see android documentation for further details) */
+    override fun describeContents() = 0
+
+    /**
+     * Saves the instance data to the given parcel.
+     * @param dest The parcel to where the data is to be saved to
+     * @param flags Not used (see android documentation for further details)
+     */
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeTypedObject(images, flags)
+        dest.writeStringList(changeKeys)
+    }
 }
--- MyMovieDB/app/src/main/kotlin/isel/pdm/demos/mymoviedb/models/MovieDetail.kt
+++ MyMovieDB/app/src/main/kotlin/isel/pdm/demos/mymoviedb/models/MovieDetail.kt
@@ -1,9 +1,28 @@
 package isel.pdm.demos.mymoviedb.models
 
+import java.util.*
+import android.os.Parcel
+import android.os.Parcelable
+
 import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * Class whose instances represent movie information obtained from the remote API.
+ *
+ * <p>As all other DTOs used in the application, the class implements the Parcelable contract
+ * thereby allowing them to be passed between Android components and, eventually, across process
+ * boundaries.</p>
+ *
+ * @property id The movie identifier
+ * @property adult boolean value indicating whether the movie is rated as adult content
+ * @property backdropPath The relative path for obtaining the movie's backdrop image
+ * @property posterPath The relative path for obtaining the movie's poster image
+ * @property homepage The movie's homepage URL
+ * @property title The movie's title
+ * @property overview The movie's description (an overview text)
+ * @property originalTitle The movie's original title
+ * @property belongsToCollection The movie collection to which the movie belongs to (if any)
+ * @property genres The genres to which the movie is related to
  */
 data class MovieDetail(
         val id: Int,
@@ -15,14 +34,143 @@ data class MovieDetail(
         val overview: String,
         @JsonProperty("original_title") val originalTitle: String,
         @JsonProperty("belongs_to_collection") val belongsToCollection: MovieCollection?,
-        val genres: Collection<Genre>
-    ) {
+        val genres: List<Genre>) : Parcelable {
 
+    /**
+     * Class whose instances represent movie collections in the enclosing movie detail instance.
+     *
+     * @property id The collection identifier
+     * @property name The collection name
+     * @property posterPath The relative path for obtaining the collection's poster image
+     * @property backdropPath The relative path for obtaining the collection's backdrop image
+     */
     data class MovieCollection(
             val id: Int,
             val name: String,
             @JsonProperty("poster_path") val posterPath: String?,
-            @JsonProperty("backdrop_path") val backdropPath: String?)
+            @JsonProperty("backdrop_path") val backdropPath: String?) : Parcelable {
 
-    data class Genre(val id: Int, val name: String)
+        companion object {
+            /** Factory of MovieCollectionqç instances */
+            @JvmField @Suppress("unused")
+            val CREATOR = object : Parcelable.Creator<MovieCollection> {
+                override fun createFromParcel(source: Parcel) = MovieCollection(source)
+                override fun newArray(size: Int): Array<MovieCollection?> = arrayOfNulls(size)
+            }
+        }
+
+        /**
+         * Initiates an instance from the given parcel.
+         * @param source The parcel from where the data is to be loaded from
+         */
+        constructor(source: Parcel) : this(
+                id = source.readInt(),
+                name = source.readString(),
+                posterPath = source.readString(),
+                backdropPath = source.readString()
+        )
+
+        /** Not used (see android documentation for further details) */
+        override fun describeContents() = 0
+
+        /**
+         * Saves the instance data to the given parcel.
+         * @param dest The parcel to where the data is to be saved to
+         * @param flags Not used (see android documentation for further details)
+         */
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.apply {
+                writeInt(id)
+                writeString(name)
+                writeString(posterPath)
+                writeString(backdropPath)
+            }
+        }
+    }
+
+    /**
+     * Class whose instances represent movie genres in the enclosing movie detail instance.
+     *
+     * @property id The genre identifier
+     * @property name The genre's name
+     */
+    data class Genre(val id: Int, val name: String) : Parcelable {
+
+        companion object {
+            /** Factory of Genre instances */
+            @JvmField @Suppress("unused")
+            val CREATOR = object : Parcelable.Creator<Genre> {
+                override fun createFromParcel(source: Parcel) = Genre(source)
+                override fun newArray(size: Int): Array<Genre?> = arrayOfNulls(size)
+            }
+        }
+
+        /**
+         * Initiates an instance from the given parcel.
+         * @param source The parcel from where the data is to be loaded from
+         */
+        constructor(source: Parcel) : this(id = source.readInt(), name = source.readString())
+
+        /** Not used (see android documentation for further details) */
+        override fun describeContents() = 0
+
+        /**
+         * Saves the instance data to the given parcel.
+         * @param dest The parcel to where the data is to be saved to
+         * @param flags Not used (see android documentation for further details)
+         */
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.writeInt(id)
+            dest.writeString(name)
+        }
+    }
+
+    companion object {
+        /** Factory of MovieDetail instances */
+        @JvmField @Suppress("unused")
+        val CREATOR = object : Parcelable.Creator<MovieDetail> {
+            override fun createFromParcel(source: Parcel) = MovieDetail(source)
+            override fun newArray(size: Int): Array<MovieDetail?> = arrayOfNulls(size)
+        }
+    }
+
+    /**
+     * Initiates an instance from the given parcel.
+     * @param source The parcel from where the data is to be loaded from
+     */
+    constructor(source: Parcel) : this(
+            id = source.readInt(),
+            adult = 1 == source.readInt(),
+            backdropPath = source.readString(),
+            posterPath = source.readString(),
+            homepage = source.readString(),
+            title = source.readString(),
+            overview = source.readString(),
+            originalTitle = source.readString(),
+            belongsToCollection = source.readTypedObject(MovieCollection.CREATOR),
+            genres = mutableListOf<Genre>().apply { source.readTypedList(this, Genre.CREATOR) }
+    )
+
+    /** Not used (see android documentation for further details) */
+    override fun describeContents() = 0
+
+    /**
+     * Saves the instance data to the given parcel.
+     * @param dest The parcel to where the data is to be saved to
+     * @param flags Not used (see android documentation for further details)
+     */
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.apply {
+            writeInt(id)
+            writeInt((if (adult) 1 else 0))
+            writeString(backdropPath)
+            writeString(posterPath)
+            writeString(homepage)
+            writeString(title)
+            writeString(overview)
+            writeString(originalTitle)
+            writeTypedObject(belongsToCollection, 0)
+            writeTypedList(genres)
+        }
+    }
 }
